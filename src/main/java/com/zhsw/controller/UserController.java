98c9056510e390d39a9bd26f2ddfde1400be0999
zhengliang <zhengliang@zhenghaosw.com>
2019-12-30 09:46:35 +0800

    修改代码

--- src/main/java/com/zhsw/controller/UserController.java
+++ src/main/java/com/zhsw/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.zhsw.controller;
 
+import com.zhsw.entity.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @Author: zhengliang
@@ -13,9 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "用户管理")
 @RestController
 public class UserController {
+
     @ApiOperation(value = "更新用户")
     @PostMapping("/updateUser")
-    public String updateUser(){
+    public String updateUser(@RequestBody User user){
         return  "修改用户";
     }
 }
